7815fddf919aa62e51947398e1491ac66051ffb5
RAZERKrakenYT <52622827+RAZERKrakenYT@users.noreply.github.com>
2022-03-23 23:19:03 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -78,7 +78,6 @@ async def on_command_error(ctx, error):
 
 ### Echoing ###
 @client.command()
-@commands.has_role("CLOUDEX™")
 async def echo(ctx):
     await ctx.channel.purge(limit=1)
     msg = ctx.message.content.split()
