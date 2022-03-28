b9ff45f0ef3a60671b777ad7172b006c81108f9b
RAZERKrakenYT <52622827+RAZERKrakenYT@users.noreply.github.com>
2022-03-29 04:23:05 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ client = commands.Bot(command_prefix = "=")
 ### Bot booting ###
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Streaming(name="ɾ Λ ʐ Ξ ɾ - on Facebook !!!", url="https://www.facebook.com/razerezzzpzzzGG"))
+    await client.change_presence(activity=discord.Streaming(name="ɾ Λ ʐ Ξ ɾ - on Facebook !!!", url="https://www.facebook.com/gaming/razerezzzpzzzGG"))
     # await client.user.edit(username="CLOUDEX™")
     print("I am on service sir !")
 
