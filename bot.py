78b333e87cb542319e4301716044a69cd43b4dc0
RAZERKrakenYT <52622827+RAZERKrakenYT@users.noreply.github.com>
2022-05-26 12:05:02 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ client = commands.Bot(command_prefix = "=")
 ### Bot booting ###
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Streaming(name="- on Twitch XEE !!!", url="https://www.twitch.tv/xeeeTV"))
+    await client.change_presence(activity=discord.Streaming(name="- on Twitch xeeeTV !!!", url="https://www.twitch.tv/xeeeTV"))
     # await client.user.edit(username="CLOUDEX™")
     print("I am on service sir !")
 
